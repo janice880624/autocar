1c16340bb89b7136f8b6eff51cbe3a1b6722896a
janice880624 <47550853+janice880624@users.noreply.github.com>
2022-01-14 00:59:28 +0800

    0114

--- car/car.js
+++
@@ -1,6 +0,0 @@
-let loc = location.href;
-let n1 = loc.length; 
-let n2 = loc.indexOf("="); 
-let device_id = decodeURI(loc.substr(n2+1, n1-n2));
-alert("Device ID:" + device_id); 
-console.log('=> ' + device_id);
