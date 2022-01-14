ec05ea156458531a447cc499c4f22005d0b31687
janice880624 <47550853+janice880624@users.noreply.github.com>
2022-01-14 22:27:46 +0800

    0114

--- car_web/car_web.js
+++ car_web/car_web.js
@@ -1,7 +1,7 @@
 let loc = location.href;
 let n1 = loc.length; 
 let n2 = loc.indexOf("="); 
-let device_id = decodeURI(loc.substr(n2+1, n1-n2));
+let device_id = decodeURI(loc.substring(n2+1, n1-n2));
 alert("Device ID:" + device_id); 
 document.write("device_id:" + device_id)
 console.log('=> ' + device_id);
